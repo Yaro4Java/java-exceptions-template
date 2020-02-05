09672fc14a5287762e55b6b7d9f8084fbabd02c8
Yaro4Java <kozlov_yv@mail.ru>
2020-02-05 23:47:39 +0300

    debugging user repository

--- src/main/java/com/epam/izh/rd/online/JavaExceptions.java
+++ src/main/java/com/epam/izh/rd/online/JavaExceptions.java
@@ -1,9 +1,53 @@
 package com.epam.izh.rd.online;
 
 
+import com.epam.izh.rd.online.entity.User;
+import com.epam.izh.rd.online.repository.UserRepository;
+
 public class JavaExceptions {
 
     public static void main(String[] args) {
+
+        {/***** DEBUGGING USER REPOSITORY *****/
+
+            System.out.println("\n***** DEBUGGING USER REPOSITORY *****\n");
+
+
+            System.out.println("1.Creating repository...");
+            UserRepository userRepository = new UserRepository();
+
+            System.out.println("The list inside repository is: " + userRepository.printInternalArray());
+
+            User admin = new User("Admin", "a");
+            User vasja = new User("Vasja", "v");
+            User styopa = new User("Styopa", "s");
+
+            System.out.println("\n2.Adding users into repository...");
+
+            userRepository.save(admin);
+            userRepository.save(vasja);
+            userRepository.save(styopa);
+
+            System.out.println("The list inside repository is: " + userRepository.printInternalArray());
+
+            System.out.println("\n3.Deleting user at the index 1 in the list of repository...");
+
+            userRepository.deleteByIndex(1);
+
+            System.out.println("The list inside repository is: " + userRepository.printInternalArray());
+            System.out.println("Size of the list is: " + userRepository.size() + " ( < internal capacity of the list = 10 ) ");
+
+            System.out.println("\n4.Trimming internal array of the list of the repository...");
+
+            userRepository.trimNullElementsOfTheInternalArrayOfTheList();
+
+            System.out.println("The list inside repository is: " + userRepository.printInternalArray());
+            System.out.println("Size of the list is: " + userRepository.size() + " ( = internal capacity of the list = 2 ) ");
+
+
+            System.out.println("\n***** END OF DEBUGGING USER REPOSITORY *****\n");
+
+        }/***** END OF DEBUGGING USER REPOSITORY *****/
 
     }
 
