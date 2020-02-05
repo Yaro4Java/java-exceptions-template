eb594ad107851d75e374059a66203b30ee98103d
Yaro4Java <kozlov_yv@mail.ru>
2020-02-06 01:11:47 +0300

    debugging user authentication service

--- src/main/java/com/epam/izh/rd/online/JavaExceptions.java
+++ src/main/java/com/epam/izh/rd/online/JavaExceptions.java
@@ -2,15 +2,19 @@ package com.epam.izh.rd.online;
 
 
 import com.epam.izh.rd.online.entity.User;
+import com.epam.izh.rd.online.exception.NotCorrectPasswordException;
+import com.epam.izh.rd.online.exception.UserNotFoundException;
 import com.epam.izh.rd.online.repository.UserRepository;
+import com.epam.izh.rd.online.service.AuthenticationService;
+import com.epam.izh.rd.online.service.CurrentUserManager;
 
 public class JavaExceptions {
 
     public static void main(String[] args) {
 
-        {/***** DEBUGGING USER REPOSITORY *****/
+        {/***** DEBUGGING USER REPOSITORY AND SERVICES *****/
 
-            System.out.println("\n***** DEBUGGING USER REPOSITORY *****\n");
+            System.out.println("\n***** DEBUGGING USER REPOSITORY AND SERVICES*****\n");
 
 
             System.out.println("1.Creating repository...");
@@ -44,10 +48,28 @@ public class JavaExceptions {
             System.out.println("The list inside repository is: " + userRepository.printInternalList());
             System.out.println("Size of the list is: " + userRepository.size() + " ( = internal capacity of the list = 2 ) ");
 
+            AuthenticationService authenticationService = new AuthenticationService(userRepository);
 
-            System.out.println("\n***** END OF DEBUGGING USER REPOSITORY *****\n");
+            System.out.println("\n5.Styopa is trying to log in...");
+            try {
+                System.out.println("Result of login is: " + authenticationService.login(styopa));
+            } catch (UserNotFoundException | NotCorrectPasswordException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Current user is: " + CurrentUserManager.getCurrentLoggedInUser());
 
-        }/***** END OF DEBUGGING USER REPOSITORY *****/
+            System.out.println("\n6.Styopa is trying to log in with wrong password...");
+            try {
+                System.out.println("Result of login is: " + authenticationService.login(new User("Styopa", "password")));
+            } catch (UserNotFoundException | NotCorrectPasswordException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Current user is: " + CurrentUserManager.getCurrentLoggedInUser());
+
+
+            System.out.println("\n***** END OF DEBUGGING USER REPOSITORY AND SERVICES *****\n");
+
+        }/***** END OF DEBUGGING USER REPOSITORY AND SERVICES*****/
 
     }
 
